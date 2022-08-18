2d90a58bcb87f190d217f12048bec13d91fb5d6f
özcan çetin <cetin445@gmail.com>
2022-08-18 18:55:25 +0300

    v1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Values from "values.js";
 function App() {
   const [color, setColor] = useState("");
   const [error, setError] = useState(false);
-  const [list, setList] = useState(new Values("#f15025").all(10));
+  const [list, setList] = useState(new Values("#f34000").all(10));
   // console.log(list);
 
   const handleSubmit = (e) => {
@@ -16,7 +16,7 @@ function App() {
       let colors = new Values(color).all(10);
       setList(colors);
     } catch (error) {
-      setError(true)
+      setError(true);
       console.log(error);
     }
   };
@@ -30,7 +30,7 @@ function App() {
             type="text"
             value={color}
             onChange={(e) => setColor(e.target.value)}
-            placeholder="#fff"
+            placeholder="#f34000"
             className={`${error} ? "error" : null`}
           />
           <button type="submit" className="btn btn-primary">
@@ -38,13 +38,18 @@ function App() {
           </button>
         </form>
       </div>
-      {
-        list?.map((color, index)=>{
-          return(
-            <SingleColor key={index} color={color}/>
-          )
-        })
-      }
+      <div className="d-flex flex-wrap container-fluid">
+        {list?.map((color, index) => {
+          return (
+            <SingleColor
+              key={index}
+              {...color}
+              index={index}
+              hexColor={color.hex}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
--- src/SingleColor.js
+++ src/SingleColor.js
@@ -1,10 +1,35 @@
-import React, { useState, useEffect } from 'react'
-import rgbToHex from './utils'
+import React, { useState, useEffect } from "react";
+import rgbToHex from "./utils";
 
-const SingleColor = ({color}) => {
-  const {rgb, weight, index, hexColor} = color
-  console.log(rgb)
-  return <h4>single color</h4>
-}
+const SingleColor = ({ rgb, weight, index, hexColor }) => {
+  const [alert, setAlert] = useState(false);
+  console.log(rgb);
+  console.log(rgb.join(","));
+  const bcg = rgb.join(",");
+  const hex = rgbToHex(...rgb);
+  const hexValue = `#${hexColor}`;
 
-export default SingleColor
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAlert(false);
+    }, 3000);
+    return () => clearTimeout(timeout);
+  }, [alert]);
+
+  return (
+    <div
+      style={{ backgroundColor: `rgb(${bcg})` }}
+      className="colorDiv d-flex flex-column justify-content-start align-items-center"
+      onClick={() => {
+        setAlert(true);
+        navigator.clipboard.writeText(hexValue);
+      }}
+    >
+     <p className='percent-value'>{weight}%</p>
+      <p className='color-value'>{hexValue}</p>
+      {alert && <p className='alert text-uppercase fs-6'>copied to clipboard</p>}
+    </div>
+  );
+};
+
+export default SingleColor;
